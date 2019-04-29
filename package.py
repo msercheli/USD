268c40d927cbf46a72a91adebb8cce1c19536cc9
Marcelo Sercheli <msercheli@gmail.com>
2019-04-29 18:55:52 -0400

    modified to reflect usd package

--- package.py
+++ package.py
@@ -2,7 +2,7 @@
 
 name = "usd"
 
-version = "19.03"
+version = "19.05"
 
 description = \
     """
@@ -14,19 +14,17 @@ authors = ["Pixar"]
 requires = [
     "boost",
     "tbb",
+    "OpenSubdiv",
     "glew",
+    "openexr",
     "python",
     "ptex",
-    "openexr",
-    "OpenSubdiv",
     "pyside",
 ]
-
-build_command = "pip install /home/marcelo/Dev/payload/numpy/numpy-1.16.2-cp27-cp27mu-manylinux1_x86_64.whl --prefix=/home/marcelo/packages/numpy/1.16.2"
 
 def commands():
     env.LD_LIBRARY_PATH.prepend("{root}/lib64")
     env.PYTHONPATH.append("{root}/lib64/python2.7/site-packages")
-    env.NUMPY_ROOT = '{root}'
+    env.USD_ROOT = '{root}'
 
-uuid = "repository.numpy"
+uuid = "repository.usdS"
